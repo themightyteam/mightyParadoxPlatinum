b5eb90d024d5d50c8550ac18d161a00f8e7bfb12
eLeDe <little_elede@hotmail.com>
2025-12-23 10:54:43 +0100

    feat: set up html version

--- html/src/ludum/mighty/paradox/client/HtmlLauncher.java
+++ html/src/ludum/mighty/paradox/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package ludum.mighty.paradox.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import ludum.mighty.paradox.MightyGameDefault;
+import ludum.mighty.paradox.game.MightyGame;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new MightyGameDefault();
+                return new MightyGame();
         }
 }
